799a33579514bae6db86cfb345eb592a889fb9d1
André Felipe <andre.vargas@estudante.ufjf.br>
2023-12-20 19:52:26 -0300

    js concertado e finalizado

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function rollDice() {
 
 // Função para obter o caminho da imagem correspondente ao valor do dado
 function getDiceImage(value) {
-    // Certifique-se de que o valor esteja dentro do intervalo de 1 a 6
+    // aleatorio de 1 a 6
     value = Math.min(Math.max(value, 1), 6);
     return Dados[value - 1];
 }
@@ -41,11 +41,11 @@ function getDiceImage(value) {
 // Função para adicionar um evento de clique às células
 function addCellClick(cellElement) {
     cellElement.addEventListener("click", function () {
-        // Verificar se a célula está vazia
+        // Verifica se a célula está vazia
         if (!cellElement.querySelector('img')) {
-            // Gerar um número aleatório para o dado
+            // número aleatório para o dado
             dice = rollDice();
-            // Obter o caminho da imagem correspondente ao valor do dado
+            // imagem correspondente 
             const diceImage = getDiceImage(dice);
             
             // Criar um elemento img e definir seu atributo src
@@ -68,31 +68,30 @@ function addCellClick(cellElement) {
                 score1 = calculateScore(cells1);
                 // Atualizar a pontuação do jogador 1 no HTML
                 score1Element.textContent = score1;
-                // Verificar se o jogo acabou
+                // Verifica se o jogo acabou
                 if (filled1 === 9) {
-                    // Mostrar a mensagem de fim de jogo
                     endGame();
                 } else {
                     // Mudar a vez para o jogador 2
                     changeTurn();
                 }
             } else {
-                // Adicionar o valor da célula ao array do jogador 2
+                // Adiciona o valor da célula ao array do jogador 2
                 cells2.push(dice);
-                // Incrementar o número de células preenchidas pelo jogador 2
+                // Incrementr o número de células preenchidas pelo jogador 2
                 filled2++;
-                // Descartar os dados do jogador 1 que tenham o mesmo valor
+                // Descarta os dados do jogador 1 que tenham o mesmo valor
                 discard(1, dice);
-                // Calcular a pontuação do jogador 2
+                // Calcula a pontuação do jogador 2
                 score2 = calculateScore(cells2);
-                // Atualizar a pontuação do jogador 2 no HTML
+                // Atualiza a pontuação do jogador 2 no HTML
                 score2Element.textContent = score2;
-                // Verificar se o jogo acabou
+                // Verifica se o jogo acabou
                 if (filled2 === 9) {
-                    // Mostrar a mensagem de fim de jogo
+                    // Mostra a mensagem de fim de jogo
                     endGame();
                 } else {
-                    // Mudar a vez para o jogador 1
+                    // Muda a vez para o jogador 1
                     changeTurn();
                 }
             }
@@ -100,7 +99,7 @@ function addCellClick(cellElement) {
     });
 }
 
-// Adicionar o evento de clique às células dos jogadores
+// Adiciona o clique dass células dos jogadores
 for (let i = 1; i <= 3; i++) {
     for (let j = 1; j <= 6; j++) {
         addCellClick(document.getElementById("cell" + i + j));
@@ -109,7 +108,7 @@ for (let i = 1; i <= 3; i++) {
 
 // Função para descartar os dados do oponente que tenham o mesmo valor
 function discard(player, value) {
-    // Verificar qual jogador está sendo afetado
+    // Verificr qual jogador está sendo afetado
     if (player === 1) {
         // Percorrer as células do jogador 1
         for (let i = 1; i <= 3; i++) {
@@ -128,12 +127,12 @@ function discard(player, value) {
             }
         }
     } else {
-        // Percorrer as células do jogador 2
+        // Percorre as células do jogador 2
         for (let i = 1; i <= 3; i++) {
             for (let j = 4; j <= 6; j++) {
                 // Pegar a célula atual
                 let cell = document.getElementById("cell" + i + j);
-                // Verificar se a célula tem o mesmo valor do dado
+                // Verifica se a célula tem o mesmo valor do dado
                 if (cell.querySelector('img') && cell.querySelector('img').alt === `dice${value}`) {
                     // Limpar a célula
                     cell.innerHTML = "";
@@ -150,35 +149,35 @@ function discard(player, value) {
 
 // Função para calcular a pontuação de um jogador
 function calculateScore(cells) {
-    // Inicializar a pontuação
+    // Inicializa a pontuação
     let score = 0;
-    // Verificar se o array de células está vazio
+    // Verifica se o array de células está vazio
     if (cells.length === 0) {
         // Retornar zero
         return score;
     }
-    // Ordenar o array de células em ordem crescente
+    // Ordena o array de células em ordem crescente
     cells.sort((a, b) => a - b);
-    // Inicializar o valor e a quantidade do dado atual
+    // Inicializa o valor e a quantidade do dado atual
     let value = cells[0];
     let count = 1;
-    // Percorrer o array de células a partir do segundo elemento
+    // Percorre o array de células a partir do segundo elemento
     for (let i = 1; i < cells.length; i++) {
-        // Verificar se o elemento atual é igual ao anterior
+        // Verifica se o elemento atual é igual ao anterior
         if (cells[i] === value) {
             // Incrementar a quantidade do dado atual
             count++;
         } else {
             // Somar a pontuação do dado atual
-            score += value * count * count; // Alterar a fórmula aqui
-            // Atualizar o valor e a quantidade do dado atual
+            score += value * count * count; 
+            // Atualiza o valor e a quantidade do dado atual
             value = cells[i];
             count = 1;
         }
     }
     // Somar a pontuação do último dado
     score += value * count * count; 
-    // Retornar a pontuação
+    // Retorna a pontuação
     return score;
 }
 
@@ -209,15 +208,12 @@ function changeTurn() {
 
 // Função para mostrar a mensagem de fim de jogo
 function endGame() {
-    // Verificar qual jogador ganhou
+    // Verifica qual jogador ganhou
     if (score1 > score2) {
-        // Mostrar a mensagem de vitória do jogador 1
         message.textContent = "Jogador 1 venceu!";
     } else if (score1 < score2) {
-        // Mostrar a mensagem de vitória do jogador 2
         message.textContent = "Jogador 2 venceu!";
     } else {
-        // Mostrar a mensagem de empate
         message.textContent = "Empate!";
     }
 }
